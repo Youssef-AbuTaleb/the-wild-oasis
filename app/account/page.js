8e6bc0f07c7aa28f7df687a9caa21dd3764f439d
Youssefabotaleb <youssef@lexmodo.com>
2024-07-26 22:25:47 +0300

    add dynamic routes and cabin details page

--- app/account/page.js
+++ app/account/page.js
@@ -2,7 +2,7 @@ export const metadata = {
   title: "Guest Area",
 };
 
-export default function page() {
+export default function Page() {
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
       Welcome %%UserName%%
